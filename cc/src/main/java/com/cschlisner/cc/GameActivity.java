da0e3f3aed0a9d288f511d8f61710693550aa680
Cole Schlisner <cschlisner@gmail.com>
2013-12-02 23:23:28 -0700

    Update GameActivity

--- cc/src/main/java/com/cschlisner/cc/GameActivity.java
+++ cc/src/main/java/com/cschlisner/cc/GameActivity.java
@@ -9,18 +9,11 @@ import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class GameActivity extends ActionBarActivity{
     private boolean inPause, startedActivity;
@@ -61,18 +54,12 @@ public class GameActivity extends ActionBarActivity{
 
         // desired fps
         private final static int 	MAX_FPS = 50;
-        // maximum number of frames to be skipped
         private final static int	MAX_FRAME_SKIPS = 5;
-        // the frame period
         private final static int	FRAME_PERIOD = 1000 / MAX_FPS;
 
-        // Surface holder that can access the physical surface
         private SurfaceHolder surfaceHolder;
-        // The actual view that handles inputs
-        // and draws to the surface
         private GameView gamePanel;
 
-        // flag to hold game state 
         private boolean running;
         public void setRunning(boolean running) {
             this.running = running;
@@ -88,55 +75,42 @@ public class GameActivity extends ActionBarActivity{
         public void run() {
             Canvas canvas;
 
-            long beginTime;		// the time when the cycle begun
-            long timeDiff;		// the time it took for the cycle to execute
-            int sleepTime;		// ms to sleep (<0 if we're behind)
-            int framesSkipped;	// number of frames being skipped 
+            long beginTime;
+            long timeDiff;
+            int sleepTime;
+            int framesSkipped;
 
             sleepTime = 0;
 
             while (running) {
                 canvas = null;
-                // try locking the canvas for exclusive pixel editing
-                // in the surface
                 try {
                     canvas = this.surfaceHolder.lockCanvas();
                     synchronized (surfaceHolder) {
                         beginTime = System.currentTimeMillis();
-                        framesSkipped = 0;	// resetting the frames skipped
-                        // update game state 
+                        framesSkipped = 0;
                         this.gamePanel.update();
-                        // render state to the screen
-                        // draws the canvas on the panel
                         this.gamePanel.render(canvas);
-                        // calculate how long did the cycle take
                         timeDiff = System.currentTimeMillis() - beginTime;
-                        // calculate sleep time
                         sleepTime = (int)(FRAME_PERIOD - timeDiff);
 
                         if (sleepTime > 0) {
-                            // if sleepTime > 0 we're OK
                             try {
-                                // send the thread to sleep for a short period
-                                // very useful for battery saving
                                 Thread.sleep(sleepTime);
                             } catch (InterruptedException e) {}
                         }
 
                         while (sleepTime < 0 && framesSkipped < MAX_FRAME_SKIPS) {
-                            // we need to catch up
-                            this.gamePanel.update(); // update without rendering
-                            sleepTime += FRAME_PERIOD;	// add frame period to check if in next frame
+                            this.gamePanel.update();
+                            sleepTime += FRAME_PERIOD;
                             framesSkipped++;
                         }
                     }
                 } finally {
-                    // in case of an exception the surface is not left in 
-                    // an inconsistent state
                     if (canvas != null) {
                         surfaceHolder.unlockCanvasAndPost(canvas);
                     }
-                }	// end finally
+                }
             }
         }
 
@@ -149,7 +123,6 @@ public class GameActivity extends ActionBarActivity{
     public class GameView extends SurfaceView implements
             SurfaceHolder.Callback {
 
-        public boolean starting;
         private int level, lives, score, coinCount, coinsCollected, fireSpeed,
                     fireCount, playerSpeed, screenWidth, screenHeight, bgColor;
         private boolean potLevel;
@@ -163,7 +136,6 @@ public class GameActivity extends ActionBarActivity{
         private Potion potion;
         public GameView(Context context) {
             super(context);
-            // adding the callback (this) to the surface holder to intercept events
             getHolder().addCallback(this);
             DisplayMetrics metrics = context.getResources().getDisplayMetrics();
             screenWidth = metrics.widthPixels;
@@ -202,10 +174,8 @@ public class GameActivity extends ActionBarActivity{
             }
             potion = new Potion(context, screenHeight, screenWidth, statusBar.height);
 
-            // create the game loop thread
             thread = new MainThread(getHolder(), this);
 
-            // make the GamePanel focusable so it can handle events
             setFocusable(true);
         }
 
